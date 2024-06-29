7810d9f082bc6ebcae78bb9a7fe2145056388d7d
adilsaid <adilsaid64@gmail.com>
2024-06-29 17:26:18 +0100

    some fixes

--- frontend/src/ContactForm.jsx
+++ frontend/src/ContactForm.jsx
@@ -1,46 +1,36 @@
 import { useState } from "react";
 
-const ContactForm = () => {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
+const ContactForm = ({ existingContact = {}, updateCallback }) => {
+    const [firstName, setFirstName] = useState(existingContact.firstName || "");
+    const [lastName, setLastName] = useState(existingContact.lastName || "");
+    const [email, setEmail] = useState(existingContact.email || "");
+
+    const updating = Object.entries(existingContact).length !== 0
 
     const onSubmit = async (e) => {
-        e.preventDefault();
+        e.preventDefault()
 
         const data = {
             firstName,
             lastName,
             email
-        };
-
-        const url = 'http://127.0.0.1:5000/create_contact';
+        }
+        const url = "http://127.0.0.1:5000/" + (updating ? `update_contact/${existingContact.id}` : "create_contact")
         const options = {
-            method: 'POST',
+            method: updating ? "PATCH" : "POST",
             headers: {
-                'Content-Type': 'application/json'
+                "Content-Type": "application/json"
             },
             body: JSON.stringify(data)
-        };
-
-        try {
-            const response = await fetch(url, options);
-            if (!response.ok) {
-                const message = await response.json();
-                alert(message); // Alert the error message
-            } else {
-                // Successful creation, optionally handle success state
-                alert('Contact created successfully!');
-                // Clear form fields if needed
-                setFirstName('');
-                setLastName('');
-                setEmail('');
-            }
-        } catch (error) {
-            console.error('Error creating contact:', error);
-            // Handle error state if fetch fails
         }
-    };
+        const response = await fetch(url, options)
+        if (response.status !== 201 && response.status !== 200) {
+            const data = await response.json()
+            alert(data.message)
+        } else {
+            updateCallback()
+        }
+    }
 
     return (
         <form onSubmit={onSubmit}>
@@ -71,9 +61,9 @@ const ContactForm = () => {
                     onChange={(e) => setEmail(e.target.value)}
                 />
             </div>
-            <button type="submit">Create Contact</button>
+            <button type="submit">{updating ? "Update" : "Create"}</button>
         </form>
     );
 };
 
-export default ContactForm;
+export default ContactForm
